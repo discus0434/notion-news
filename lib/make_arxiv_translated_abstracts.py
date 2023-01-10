8ee55ed1dffcc2735977d65935745ea5063fc7b0
discus0434 <discus0434@gmail.com>
2023-01-10 17:34:12 +0000

    update

--- lib/make_arxiv_translated_abstracts.py
+++ lib/make_arxiv_translated_abstracts.py
@@ -53,7 +53,7 @@ def make_arxiv_translated_abstracts(
         total=len(arxiv_urls),
     ):
         inputs = tokenizer(info.summary, return_tensors="pt")
-        outputs = model.generate(**inputs)
+        outputs = model.generate(**inputs, max_length=512)
         translated = tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
         subprocess.run(
