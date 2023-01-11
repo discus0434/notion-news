85210b654ca6abaa508fdf90978890fe16879602
Nanka <66945496+discus0434@users.noreply.github.com>
2023-01-11 11:23:09 +0900

    Update remove_all_blocks_in_database.py

--- lib/remove_all_blocks_in_database.py
+++ lib/remove_all_blocks_in_database.py
@@ -4,8 +4,8 @@ from notion_client import Client
 def remove_all_blocks_in_database(client: Client, database_id: str) -> None:
     """Remove all blocks in database.
 
-    Arguments
-    ---------
+    Parameters
+    ----------
     client : Client
         notion client.
     database_id : str
